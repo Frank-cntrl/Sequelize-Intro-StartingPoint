5bc2cdebc247b03469829a773d33831be9f1c7e9
Franccesco Petta <franccescopettamuller@gmail.com>
2025-06-27 14:51:07 -0400

    starting point

--- api/tasks.js
+++ api/tasks.js
@@ -6,18 +6,56 @@ const { Task, User } = require("../database");
 // This time, use your newly created Sequelize models instead of the dummy database
 
 // GET all tasks
-router.get("/", async (req, res) => {
-  // Replace this with your code!
-  res.status(501).send("Not implemented");
+router.get("/", async(req, res) => {
+  try{
+  const task = Task.findAll();
+  res.json(task);
+  }catch (error){
+    res.status(500).send({error: error.message});
+  }
 });
 
 // GET a single task by id
 
 // Patch a task by id
+router.patch("/:id", async(req,res) => {
+  try{
+  const id = Number(req.params.id);
+  const task = req.body;
+  const updatedTask = Task.update(id, task);
+  res.json(updatedTask);
+  }catch (error){
+    res.status(500).send({error: error.message});
+  }
+});
 
 // Delete a task by id
+router.delete("/:id", async(req, res) => {
+  try{
+  const id = Number(req.params.id);
+  const removeTask = Task.delete(id);
+  res.status(200).json(removeTask);
+  }catch (error){
+    res.status(500).send({error: error.message});
+  }
+});
+
 
 // Create a new task
+router.post("/", async(req, res) => {
+  try{
+  const { title, description, userId } = req.body;
+  const newTask = Task.create({
+    title,
+    description,
+    completed: false,
+    userId: Number(userId),
+  });
+  res.status(201).json(newTask);
+  }catch (error){
+    res.status(500).send({error: error.message});
+  }
+});
 
 module.exports = router;
 
--- database/db.js
+++ database/db.js
@@ -2,9 +2,9 @@ const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/ttp_backend",
+  process.env.DATABASE_URL || "postgres://localhost:5432/to-do-list",
   {
-    logging: false, // comment this line to enable logging
+    //logging: false, // comment this line to enable logging
   }
 );
 
